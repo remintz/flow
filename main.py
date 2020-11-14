02b376f360e6df74bdabb9cb8f108213981f2809
Renato Mintz <remintz@gmail.com>
2020-11-14 17:07:42 -0300

    Gráficos

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 
 import time
-from typing import List
+from typing import Dict, List
 from dataclasses import dataclass
 import random
 
 import blessed
+import pandas as pd 
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib
+
+
+COLUMN_WIDTH = 14
+COLUMN_HEIGHT = 20
+COLUMNS_TOP_OFFSET = 2
+COLUMN_TITLES_LEFT_OFFSET = 4
+MAX_TASKS_PER_COLUMN = 7
+TASK_WIDTH = 10
+TASK_HEIGHT = 3
+FIRST_TASK_TOP_OFFSET = 4
+FIRST_TASK_LEFT_OFFSET = 2
 
 @dataclass
 class Task:
@@ -23,6 +38,99 @@ class Column:
     number: int
     tasks: List[Task]
 
+
+@dataclass
+class KPIs:
+    clock: int
+    lead_time: float
+    cycle_time: float
+    finished_tasks: int
+    wip: int
+
+class Statistics:
+    def __init__(self):
+        self._series = {}
+
+    def add(self, kpis: KPIs):
+        self._series[kpis.clock] = kpis
+
+    def plot_lead_time(self, id: int):
+        lead_time = []
+        for kpi in self._series.values():
+            lead_time.append(kpi.lead_time)
+        plt.plot(lead_time)
+        plt.show()
+        plt.savefig(f'lead_time_{id}.png')
+        
+
+class Draw:
+    def __init__(self, terminal):
+        self._term = terminal
+        print(self._term.home + self._term.clear)
+
+    def redraw(self, columns: List[Column], tasks: List[Task], kpis: KPIs):
+        print(self._term.home + self._term.clear)
+        self._columns = columns
+        num_columns = len(self._columns)
+        self._draw_empty_columns(num_columns, COLUMN_WIDTH)
+        col_number = 0
+        for col in self._columns:
+            self._draw_col_title(col_number, col)
+            line_number = 0
+            for task in col.tasks:
+                if line_number < MAX_TASKS_PER_COLUMN:
+                    self._draw_task(col_number, line_number, task)
+                line_number += 1
+            col_number += 1
+        self._draw_kpis(kpis)
+
+    def wait_input(self):
+        self._print_xy(0, 30, '')
+        input('->')
+
+    def _print_xy(self, x, y, char):
+        print(self._term.move_xy(x, y) + char)
+
+    def _draw_square(self, left, top, width, height):
+        self._print_xy(left, top, '+')
+        self._print_xy(left + width, top, '+')
+        self._print_xy(left, top + height, '+')
+        self._print_xy(left + width, top + height, '+')
+        for x in range(left + 1, left + width):
+            self._print_xy(x, top, '-')
+            self._print_xy(x, top + height, '-')
+        for y in range(top + 1, top + height):
+            self._print_xy(left, y, '|')
+            self._print_xy(left + width, y, '|')
+
+    def _draw_vertical_line(self, column, top, height, char):
+        for i in range(height):
+            print(self._term.move_xy(column, i + top) + char)
+
+    def _draw_task(self, column, height, task: Task):
+        top = height * TASK_HEIGHT + FIRST_TASK_TOP_OFFSET
+        left = column * COLUMN_WIDTH + FIRST_TASK_LEFT_OFFSET
+        self._draw_square(left, top, TASK_WIDTH, TASK_HEIGHT)
+        self._print_xy(left + 1, top + 1, str(task.name))
+        if task.finished > 0:
+            out = 'finished'
+        elif task.time_left > 0:
+            out = str(task.time_left)
+        else:
+            out = 'waiting'
+        self._print_xy(left + 1, top + 2, out)
+
+    def _draw_empty_columns(self, number, height):
+        for i in range(1, number):
+            self._draw_vertical_line(i * COLUMN_WIDTH, COLUMNS_TOP_OFFSET, height, '|')
+
+    def _draw_col_title(self, col_number, column: Column):
+        self._print_xy(col_number * COLUMN_WIDTH + COLUMN_TITLES_LEFT_OFFSET, COLUMNS_TOP_OFFSET, column.name)
+
+    def _draw_kpis(self, kpis: KPIs):
+        self._print_xy(0, 0, f'Clock: {kpis.clock:4d} Lead Time: {kpis.lead_time:5.2f} ' 
+            + f'Cycle Time: {kpis.cycle_time:5.2f} Finished: {kpis.finished_tasks:4d} WIP: {kpis.wip:2d}')
+
 class Game:
     def __init__(self, terminal, stages, cycle_time, min_time, max_time, wip, bottleneck_factor):
         self._tasks = []
@@ -30,10 +138,7 @@ class Game:
         self._running = False
         self._columns: List[Column] = []
         self._stages = stages
-        self._term = terminal
-        print(self._term.home + self._term.clear)
         self._number_of_tasks = 0
-        self._next_task_name = 'A'
         self._min_time = min_time
         self._max_time = max_time
         self._clock = 0
@@ -43,15 +148,18 @@ class Game:
         self._wip = wip
         self._finished_tasks = 0
         self._bottleneck_factor = bottleneck_factor
+        self._draw = Draw(terminal)
+        self._statistics = Statistics()
+        self._plot_interval = 100
 
         col_number = 0
         for i in range(stages):
-
-            column = Column(f'DOING {i}', False, col_number, [])
+            letter = chr(i + ord('A'))
+            column = Column(f'{letter} DOING', False, col_number, [])
             self._columns.append(column)
             col_number += 1
 
-            column = Column(f'DONE {i}', True, col_number, [])
+            column = Column(f'{letter} DONE', True, col_number, [])
             self._columns.append(column)
             col_number += 1
 
@@ -63,7 +171,11 @@ class Game:
             self._clock += 1
             self._wait()
             self._tick()
-            self._redraw()
+            kpis = KPIs(self._clock, self._avg_lead_time, self._avg_cycle_time, self._finished_tasks, self._wip)
+            self._draw.redraw(self._columns, self._tasks, kpis)
+            self._statistics.add(kpis)
+            if self._clock % self._plot_interval == 0:
+                self._statistics.plot_lead_time(self._clock)
 
     def _calculate_cfd(self):
         # count # of tasks in each column
@@ -102,8 +214,7 @@ class Game:
         if self._cycle_time > 0:
             time.sleep(self._cycle_time)
         else:
-            self._print_xy(0, 30, '')
-            input('->')
+            self._draw.wait_input()
 
     def _choose_time(self, bottleneck_factor):
         if self._min_time == self._max_time:
@@ -112,56 +223,6 @@ class Game:
             min_time = int(self._min_time * bottleneck_factor)
             max_time = int(self._max_time * bottleneck_factor)
             return random.randrange(min_time, max_time)
-
-    def _print_xy(self, x, y, char):
-        print(self._term.move_xy(x, y) + char)
-
-    def _draw_square(self, left, top, width, height):
-        self._print_xy(left, top, '+')
-        self._print_xy(left + width, top, '+')
-        self._print_xy(left, top + height, '+')
-        self._print_xy(left + width, top + height, '+')
-        for x in range(left + 1, left + width):
-            self._print_xy(x, top, '-')
-            self._print_xy(x, top + height, '-')
-        for y in range(top + 1, top + height):
-            self._print_xy(left, y, '|')
-            self._print_xy(left + width, y, '|')
-
-    def _draw_vertical_line(self, column, top, height, char):
-        for i in range(height):
-            print(self._term.move_xy(column, i + top) + char)
-
-    def _draw_task(self, column, height, task: Task):
-        top = height * 4 + 4
-        left = column * 15 + 3
-        self._draw_square(left, top, 10, 3)
-        self._print_xy(left + 1, top + 1, str(task.name))
-        self._print_xy(left + 1, top + 2, str(task.time_left))
-
-    def _draw_empty_columns(self, number, height):
-        for i in range(1, number):
-            self._draw_vertical_line(i * 15, 2, height, '|')
-
-    def _draw_col_title(self, col_number, column: Column):
-        self._print_xy(col_number * 15 + 4, 2, column.name)
-
-    def _draw_kpis(self, clock, avg_lead_time, avg_cycle_time, finished_tasks):
-        self._print_xy(0, 0, f'Clock: {clock:4d} Lead Time: {avg_lead_time:5.2f} Cycle Time: {avg_cycle_time:5.2f} Finished: {finished_tasks:4d}')
-
-    def _redraw(self):
-        print(self._term.home + self._term.clear)
-        self._draw_empty_columns(self._stages * 2, 20)
-        col_number = 0
-        for col in self._columns:
-            self._draw_col_title(col_number, col)
-            line_number = 0
-            for task in col.tasks:
-                if line_number < 7:
-                    self._draw_task(col_number, line_number, task)
-                line_number += 1
-            col_number += 1
-        self._draw_kpis(self._clock, self._avg_lead_time, self._avg_cycle_time, self._finished_tasks)
 
     def _can_move_to_next_action(self, col_number):
         # not the last column... check if wip for next stage is ok
@@ -242,7 +303,7 @@ class Game:
 
 if __name__ == "__main__":
     term = blessed.Terminal()
-    game = Game(term, 3, 0, 5, 8, 0, 1.5)
+    game = Game(term, 3, 0, 5, 15, 2, 1.30)
     game.start()
 
 
